11016932754bda7cb66d65a5b011ae5b63c59bbb
hemish718 <hemishsuthar1@gmail.com>
2026-05-30 13:49:25 +0530

    Fixed urls.py - removed duplicate urlpatterns

--- GymFit/urls.py
+++ GymFit/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/',include('user.urls') ),
-    path('', lambda request: redirect('user/login/')),
+    path('',include('user.urls') ),
     
 ]
