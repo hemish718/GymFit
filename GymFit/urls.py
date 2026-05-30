3fa75d6c64709045762df75a6adfca5e67db90f2
hemish718 <hemishsuthar1@gmail.com>
2026-05-30 13:45:36 +0530

    Fix: Redirect root URL to login

--- GymFit/urls.py
+++ GymFit/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/',include('user.urls') ),
-    path('',include('user.urls') ),
+    path('', lambda request: redirect('user/login/')),
     
 ]
